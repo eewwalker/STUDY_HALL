7163e16223f9852999545416075dd8f890a10480
Emily Walker <ewalker415@gmail.com>
2024-06-19 17:10:02 -0700

    added routing for app. Added Nav and a Notes component to add notes about study plan

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,28 @@
+import { NavLink, Outlet } from 'react-router-dom';
 import './App.css';
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
-import { Task } from './types/task';
-import TaskDetail from './components/TaskDetail';
+import Notes from './components/Notes';
 
-/**
- * Component App
- * State: tasksArr [{id: number, title: 'string', time: number}, {}...]
- *        activeTaskId number | null
- * Props: None
- *
- */
+//added end to NavLink- will only be highlighted when at /tasks not /tasks/23 etc..
 export default function App() {
-  const [tasksArr, setTasksArr] = useState<Task[]>([]);
-  const [activeTaskId, setActiveTaskId] = useState<number | null>(null);
-  //useRef to avoid making extra fetch to API for data
-  const fetched = useRef(false);
-
-  useEffect(()=>{
-      async function fetchTasks() {
-        try{
-          const data = await fetch(`http://localhost:3000/tasks`);
-          const jsonData = await data.json();
-          setTasksArr(jsonData)
-          fetched.current = true;
-        }catch(error){
-          console.error('Error fetching tasks', error)
-        }
-      }
-      if (!fetched.current) {
-        fetchTasks()
-      }
-
-  },[])
-
-  const activeTask = tasksArr.find(t => activeTaskId === t.id);
-
-  //Handles updating state when title is changed in input field
-  function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
-    const updatedTitle = e.target.value;
-    setTasksArr(prevTasks => prevTasks.map(t => {
-      if (t.id === activeTaskId) {
-        return { ...t, title: updatedTitle };
-      }
-      return t;
-    }));
-  }
-
-  function handleSelectTask(id: number) {
-    setActiveTaskId(id);
-  }
-
   return (
-    <div className='container mt-7 p-7 mx-auto bg-[#d4dffb]'>
-      <h2 className="text-5xl text-[#ec1831] underline decoration-indigo-500/30 text-center">Study Hall</h2>
-      <ul className="flex flex-col gap-2 my-3">
-        {tasksArr.map(t => (
-          <li key={t.id} className='flex m-1 cursor-pointer' onClick={() => handleSelectTask(t.id)}>
-            <span className='text-[#2e3192] border-double border-4 border-[#19b5ff]  rounded-l p-2'>{t.title}</span>
-            <span className="p-2 ml-3 border-dotted border-2 border-indigo-600 rounded-lg ">{t.time} {t.time === 1 ? 'hr' : 'hrs'}</span>
-          </li>
-        ))}
+    <>
+    <nav className='bg-[#e1bf42] p-1 mt-2'>
+      <ul className='flex justify-center gap-20 my-3 text-3xl text-[#2e3192] font-semibold uppercase'>
+        <li>
+          <NavLink to='/tasks' end>Study Hall</NavLink>
+        </li>
       </ul>
-      {activeTask &&
-        <TaskDetail task={activeTask} titleChange={handleTitleChange} />
-      }
-
+    </nav>
+    <div className='mt-7 p-7 container mx-auto flex justify-between gap-6 text-[#19b5ff] bg-[#d4dffb] text-3xl'>
+      <div className='flex-1'>
+        {/* renders child routes */}
+        <Outlet />
+      </div>
+      <div className='flex-1'>
+       <Notes/>
+      </div>
     </div>
+    </>
+
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import TaskList from './components/TasksList.tsx';
+import TaskDetail from './components/TaskDetail.tsx';
+import { NoteProvider } from './context/NoteContext.tsx';
+import TaskForm from './components/TaskForm.tsx';
+
+//create router config => [route objs{path: root, children: [{routes}{}..]}]
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      //when root is acessed/rendered navigate directly to dashboard
+      {index: true, element: <Navigate replace to='/tasks'/>},
+      {path: '/tasks', element: <TaskList/>},
+      {path: '/tasks/:id', element: <TaskDetail />},
+      {path: '/tasks/create', element: <TaskForm/>}
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NoteProvider>
+   <RouterProvider router = {router}/>
+    </NoteProvider>
   </React.StrictMode>,
 )
